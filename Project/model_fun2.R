05f977c064c44a0dbeac8d08507bf43a36a272f3
Aphyllanthes <nele.stackelberg@gmx.de>
2018-09-30 13:28:06 +0200

    update report

--- Project/model_fun2.R
+++ Project/model_fun2.R
@@ -16,65 +16,70 @@ inp_data <- raw_data_2 %>%
   select(ch) %>% 
   mutate(freq = 1)
 
-## model function ------------
-Closed_model <- function(
-  formula, inp_data, name = "", predictor = ""
-) {
-  inp_data_process1 <- process.data(inp_data, model = "Closed")
-  inp_design1 <- RMark::make.design.data(inp_data_process1)
-  
-  if(is.numeric(predictor)){
-    inp_design1$p$predictor <- predictor
-    inp_design1$c$predictor <- predictor[2:length(predictor)]
-  }
-  model.parameters = list(p = formula)
-  m0 <- RMark::mark(data = inp_data_process1, 
-                    ddl = inp_design1, 
-                    model = "Closed",
-                    model.parameters = model.parameters
-                    )
-                    
-  res <- m0$results$derived$`N Population Size`
-  res <- res %>% 
-    mutate(name = name, AIC = m0$results$AICc, modeltype = "Closed")
-  if(exists("results")) {
-    results <<- rbind(results, res)
-  } else {results <<- res}
-  return(m0)
-}
+## Popan modell
+inp_data_process1 <- process.data(inp_data, model = "POPAN")
+inp_design1 <- RMark::make.design.data(inp_data_process1)
 
-#1 Closed models ###############
+# inp_design1$p$predictor <- predictor
+# inp_design1$c$predictor <- predictor[2:length(predictor)]
 
-#1.1 only intercept
-m0_result <- Closed_model(
-  formula = list(formula = ~1, share = TRUE), 
-  inp_data = inp_data, name = "m0"
+model.parameters = list(p = list(formula=~1 ),
+                        Phi = list(formula=~1 ), 
+                        pent = list(formula=~1 ))
+mPOPAN <- RMark::mark(data = inp_data_process1, 
+                  ddl = inp_design1, 
+                  model = "POPAN",
+                  model.parameters = model.parameters
 )
 
-##1.2 model mt: The session number is a factorial number
-mt_result <- Closed_model(
-  formula = list(formula = ~1 + time, share = TRUE), 
-  inp_data = inp_data, name = "mt"
+## 
+model.parameters = list(p = list(formula=~1 ),
+                        Phi = list(formula=~1 , fixed=1), 
+                        pent = list(formula=~1, fixed=0 ))
+mPOPAN2 <- RMark::mark(data = inp_data_process1, 
+                      ddl = inp_design1, 
+                      model = "POPAN",
+                      model.parameters = model.parameters
 )
 
-##1.3 model mtb: different probability for recaptures
-# mtb_result <- Closed_model(
-#   formula = list(formula = ~1 + time + c, share = T), ## share = T used to be working but now is not...  
-#   inp_data = inp_data, name = "mtb"
-# )
-
-##1.4- model mTrend: Trend of 
-mtrend_result <- Closed_model(
-  formula = list(formula = ~1 + Time, share = TRUE), 
-  inp_data = inp_data, name = "mtrend"
+## open with time
+model.parameters = list(p = list(formula=~1 + time ),
+                        Phi = list(formula=~1), #probability of survival
+                        pent = list(formula=~1)) # probability of entrance
+mPOPANt <- RMark::mark(data = inp_data_process1, 
+                       ddl = inp_design1, 
+                       model = "POPAN",
+                       model.parameters = model.parameters
 )
 
-##1.5 model with weather
-mw_result <- Closed_model(
-  formula = list(formula = ~1 + predictor, share = TRUE), 
-  inp_data = inp_data, name = "mw",
-  predictor = c(0.5, .3, .2, .8, .4, .9, 1, 1)
+## mPopan weather
+inp_design1$p$weather <- c(0.5, .3, .2, .8, .4, .9, 1, 1)
+inp_design1$c$weather <- c( .3, .2, .8, .4, .9, 1, 1)
+
+model.parameters = list(p = list(formula=~1 + weather),
+                        Phi = list(formula=~1), #probability of survival
+                        pent = list(formula=~1)) # probability of entrance
+mPOPANw <- RMark::mark(data = inp_data_process1, 
+                       ddl = inp_design1, 
+                       model = "POPAN",
+                       model.parameters = model.parameters
 )
+
+#pradlamda
+inp_data_process1 <- process.data(inp_data, model = "Pradlambda")
+inp_design1 <- RMark::make.design.data(inp_data_process1)
+
+model.parameters = list(p = list(formula=~1),
+                        Phi = list(formula=~1), #probability of survival
+                        Lambda = list(formula=~1)) # probability of entrance
+mPradLambda <- RMark::mark(data = inp_data_process1, 
+                       ddl = inp_design1, 
+                       model = "Pradlambda",
+                       model.parameters = model.parameters
+)
+
+
+
 
 ## model function ------------
 model_fun <- function(
@@ -145,70 +150,23 @@ m0h_result <- model_fun(
   inp_data = inp_data, name = "m0h"
 )
 
+mtb_result <- model_fun(
+  "FullHet", 
+  formula = list(formula = ~1 + c, share = TRUE), 
+  inp_data = inp_data, name = "mtb"
+)
+
 collect.models()
 
 ## Also try POPAN model (open population)
 #to test for closure violation. use model to explore the closure violation. 
 #phi
 
-
-#pent p of entrance
-p=list(formula=~1 )
-Phi=list(formula=~1) #probability of survival
-pent=list(formula=~1) # probability of entrance
-
-mPOPAN_result <- model_fun(
-  modeltype = "POPAN",
-  formula_p = p, formula_phi = Phi, formula_pent = pent, 
-  inp_data = inp_data, name = "mpopan", addresults=F)
-
 mPOPAN_result$results$derived$`Gross N* Population Size`
 mPOPAN_result$results$derived
 
 
-Phi=list(formula=~1, fixed=1) #probability of survival
-pent=list(formula=~1, fixed=0) # probability of entrance
-
-mPOPAN2_result <- model_fun(
-  modeltype = "POPAN", formula_p = p, formula_phi = Phi ,
-  formula_pent = pent, addresults=F,
-  inp_data = inp_data, name = "mpopan2")
-
 mPOPAN2_result$results$derived$`Gross N* Population Size`
-
-
-# open with time
-p=list(formula=~1 + time )
-Phi=list(formula=~1) #probability of survival
-pent=list(formula=~1 ) # probability of entrance
-
-mPOPANtime_result <- model_fun(
-  modeltype = "POPAN",
-  formula_p = p, formula_phi = Phi ,
-  formula_pent = pent, addresults=F,
-  inp_data = inp_data, name = "mpopan_t")
-
-mPOPANtime_result$results$derived$`Gross N* Population Size`
-
-## not working:
-p=list(formula=~1 + predictor )
-
-mPOPANw_result <- model_fun(
-  modeltype = "POPAN",
-  predictor = c(0.5, .3, .2, .8, .4, .9, 1, 1),
-  formula_p = p, formula_phi = Phi ,
-  formula_pent = pent, addresults=F,
-  inp_data = inp_data, name = "mpopan_w")
-
-#pradlamda
-p=list(formula=~1)
-Phi=list(formula=~1) #probability of survival
-Lambda=list(formula=~1) # probability of entrance
-
-mPradlambda_result <- model_fun(
-  modeltype = "Pradlambda",
-  formula_p = p, formula_phi = Phi, formula_lambda = Lambda, 
-  inp_data = inp_data, name = "mPradlambda", addresults = F)
 
 ## Test of clousure. Capture software on line. 
 
@@ -224,6 +182,17 @@ junk <- dir(pattern=".out")
 file.remove(junk)
 junk <- dir(pattern=".inp")
 file.remove(junk)
+
+junk <- dir(path = "Project/", pattern=".tmp")
+file.remove(paste0("Project/",junk))
+junk <- dir(path = "Project/", pattern=".vcv")
+file.remove(paste0("Project/",junk))
+junk <- dir(path = "Project/", pattern=".res")
+file.remove(paste0("Project/",junk))
+junk <- dir(path = "Project/", pattern=".out")
+file.remove(paste0("Project/",junk))
+junk <- dir(path = "Project/", pattern=".inp")
+file.remove(paste0("Project/",junk))
 
 ## Notes: ###################
 # dying of
